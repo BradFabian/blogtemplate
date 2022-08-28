5bb9cc02c62bf5825ce6ac573991dac1bfbe5a4d
brad fabian <bradleofabian@gmail.com>
2022-08-28 13:23:05 -0400

    added an mobile nav hamburger

--- components/header.js
+++ components/header.js
@@ -9,9 +9,11 @@ class Header extends HTMLElement {
         
         <header>
         <div class="header-brand">MkdirCode</div>
-      
+        <!-- Hamburger icon -->
+        <input class="side-menu" type="checkbox" id="side-menu"/>
+        <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
         <nav class="header__nav">
-            <ul role="list">
+            <ul class="menu" role="list">
                 <li class="header__nav-item"><a class="header__nav-item-link" href='./'>Home</a></li>
                 <li class="header__nav-item"><a  class="header__nav-item-link" href='./about.html'>About</a></li>
                 <li class="header__nav-item"><a class="header__nav-item-link" href='./contact.html'>Contact</a></li>
